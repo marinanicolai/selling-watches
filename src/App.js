10e0dc1be297a8dac59848741753f9e2d7d7f957
marina <nicolaimarina@gmail.com>
2022-09-05 10:26:41 -0400

    First commit.

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <p>Selling watches</p>
+    </div>
+  );
+}
+
+export default App;
